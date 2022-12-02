ad436965de52c6b44e24bd7057be3a3f1f9fa7ef
david9805 <davids.ariasa@ecci.edu.co>
2022-12-02 16:48:28 -0500

    OCTAVOS DE FINAL

--- src/controllers/contenedor.controller.js
+++ src/controllers/contenedor.controller.js
@@ -62,6 +62,7 @@ export const listContenedor = async (req,res) => {
         const pool = await getConnection();
         prediccion = await pool.request().        
         input("IDCLIENTE",sql.Numeric,result.recordset[0].IDCLIENTE).
+        input("GRUPO",sql.VarChar,'OCTAVOS').
         query(queries.existePrediccion);
         let fecha = new Date();
 
@@ -75,6 +76,7 @@ export const listContenedor = async (req,res) => {
 
             prediccion = await pool.request().        
             input("IDCLIENTE",sql.Numeric,result.recordset[0].IDCLIENTE).
+            input("GRUPO",sql.VarChar,'OCTAVOS').
             query(queries.existePrediccion);
         }
 
